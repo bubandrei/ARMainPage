27b6f3d10af6e3acae842877e962a0e73de54de5
a.bubelev <bubeleu@gmail.com>
2022-07-09 20:52:04 +0300

    fix problem

--- armp/src/components/navbar/Navbar.jsx
+++ armp/src/components/navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import stNavbar from './Navbar.module.css';
 
 const Navbar = () => {
   return (
@@ -9,7 +11,13 @@ const Navbar = () => {
         <div>О нас</div>
         <a href="">Контакты</a>
         <div>Блог</div>
+        <button>
+          <FaTimes/>
+        </button>
       </nav>
+      <button>
+        <FaBars/>
+      </button>
     </header>
   );
 };
