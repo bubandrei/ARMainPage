2cbf50e3416d34f3285556af826cb475c4d240a7
a.bubelev <bubeleu@gmail.com>
2022-07-09 21:44:33 +0300

    css for nav

--- armp/src/components/navbar/Navbar.jsx
+++ armp/src/components/navbar/Navbar.jsx
@@ -4,24 +4,29 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import stNavbar from "./Navbar.module.css";
 
 const Navbar = () => {
-  
   const navRef = useRef();
   const showNavbar = () => {
     navRef.current.classlist.toggle("responsive_nav");
   };
   return (
     <header>
-      <nav ref={navRef}>
+      <nav className={stNavbar.main} 
+      ref={navRef}>
         <a>Коталог домов</a>
         <a>Услуги</a>
         <a>О нас</a>
         <a href="">Контакты</a>
         <a>Блог</a>
-        <button onClick={showNavbar}>
+        <button
+          className={(stNavbar.nav_btn, stNavbar.nav_btn_close)}
+          onClick={showNavbar}
+        >
           <FaTimes />
         </button>
       </nav>
-      <button onClick={showNavbar}>
+      <button 
+      className={stNavbar.nav_btn}
+      onClick={showNavbar}>
         <FaBars />
       </button>
     </header>
