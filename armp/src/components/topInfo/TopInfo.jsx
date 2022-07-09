b370c0e2940a3767462ce03088fe6c7489e35f72
a.bubelev <bubeleu@gmail.com>
2022-07-09 23:40:10 +0300

    grid for top

--- armp/src/components/topInfo/TopInfo.jsx
+++ armp/src/components/topInfo/TopInfo.jsx
@@ -5,20 +5,24 @@ import phone from "../../assets/images/Group_1071.png";
 import telegramm from "../../assets/images/Group_1279.png";
 import viber from "../../assets/images/Group_1280.png";
 import simplePhone from "../../assets/images/Group_1278.png";
+import "./TopInfo.css";
 
 const TopInfo = () => {
   return (
-    <div>
-      <img src={logo} />
-      <div>Пн-Сб: c 10:00 до 20:00 Выходной: воскресенье</div>
-      <div>
+    <div className="wrap">
+      <img src={logo} className="logo" />
+      <div className="workHour">
+        <div>Пн-Сб: c 10:00 до 20:00</div>
+        <div>Выходной: воскресенье</div>
+      </div>
+      <div className="maket">
         <img src={box} />
         Заказать 3D-макет дома
       </div>
-      <div>
-        <img src={phone} />
-        <div>+7 (915) 168-55-50</div>
-        <div>
+      <div className="phoneWrapper">
+        <img src={phone} className='item1'/>
+        <div className="phone item2">+7 (915) 168-55-50</div>
+        <div className='item3'>
           <img src={simplePhone} />
           <img src={telegramm} />
           <img src={viber} />
