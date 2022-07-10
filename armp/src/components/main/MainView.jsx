5f66c9b46cf305016154f6194f12f67c7bf3bddf
a.bubelev <bubeleu@gmail.com>
2022-07-10 14:42:43 +0300

    main view fill

--- armp/src/components/main/MainView.jsx
+++ armp/src/components/main/MainView.jsx
@@ -7,28 +7,37 @@ import "./MainView.css";
 const MainView = () => {
   return (
     <div className="backImage">
-      <div>
+      <div className="title">
         Мы знаем о строительстве все, каждый новый проект — это отдельная
         история
       </div>
-      <div>
+      <div className="mainCover">
         Здесь мы делимся с вами историями, уникальным пытом, реальными кейсами и
         видео-обзорами. Это дает нам стимул развиваться и держать руку на
         пульсе, а вам позволяет наблюдать за нашими рабочими процессами и
         учиться вместе с нами.
       </div>
       <div className="flexIcons">
-        <div>
+        <div className="article">
           <img src={article} />
           <div>Кейсы</div>
         </div>
-        <div>
+        <div className="portfolio">
           <img src={portfolio} />
           <div>Видеоблог</div>
         </div>
-        <div>
+        <div className="video">
           <img src={video} />
           <div>Полезные статьи</div>
+        </div>
+      </div>
+      <div>
+        <input type="text" placeholder="Ваше имя" />
+        <input type="number" placeholder="Номер телефона" />
+        <input type="submit" placeholder="Получить консультацию" />
+        <div>
+          Ищите профессионалов? Оставьте заявку и мы проконсультируем вас
+          абсолютно бесплатно!
         </div>
       </div>
     </div>
