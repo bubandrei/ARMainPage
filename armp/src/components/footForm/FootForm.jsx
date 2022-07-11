10c39706fffb16a40d5c7e3fc3fb60d251d7a933
a.bubelev <bubeleu@gmail.com>
2022-07-11 12:59:01 +0300

    form

--- armp/src/components/footForm/FootForm.jsx
+++ armp/src/components/footForm/FootForm.jsx
@@ -9,11 +9,14 @@ const FootForm = () =>
             <div className="homeImg">
                 <img src={ home } />
             </div>
-            <div className="styleForm">
-                <form action="">
-                    <input type="text" />
-                    <input type="text" />
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
+            <div className="wrapForm">
+                <form action="" className="styleForm">
+                    <div className="titleFootForm">Хотите с нами сотрудничать?</div>
+                    <div className="textFootForm">Заполните форму и мы перезвоним вам в течение 30 минут!</div>
+                    <input type="text" placeholder="Ваше имя"/>
+                    <input type="text" placeholder="Номер телефона"/>
+                    <textarea name="" id="" cols="30" rows="10" placeholder="Ваше сообщение (необязательно)"></textarea>
+                    <input type="submit" name="" id="" className="btnFootForm"/>
                 </form>
             </div>
 
