758a77bcbf0893701548b12e61f1d4c778fefbe8
a.bubelev <bubeleu@gmail.com>
2022-07-15 12:00:40 +0300

    ready

--- armp/src/components/navbar/Navbar.jsx
+++ armp/src/components/navbar/Navbar.jsx
@@ -12,12 +12,20 @@ const Navbar = () =>
   };
   return (
     <header>
-        <div className="padLeft"><a href="#">Коталог домов <img src={arrows}/></a></div>
-        <div className="padLeft"><a href="">Услуги <img src={arrows}/></a></div>
-        <div className="padLeft"><a href="">О нас <img src={arrows}/></a></div>
-        <div className="padLeft"><a href="">Контакты <img src={arrows}/></a></div>
-        <div className="padLeft"><a href="">Блог <img src={arrows}/></a></div>
-        {/* <button
+      <div className="padLeft"><a href="#">Коталог домов <img src={ arrows } /></a></div>
+      <div className="padLeft"><a href="">Услуги <img src={ arrows } /></a></div>
+      <div className="padLeft"><a href="">О нас <img src={ arrows } /></a></div>
+      <div className="padLeft"><a href="">Контакты <img src={ arrows } /></a></div>
+      <div className="padLeft blogHover"><a href="">Блог <img src={ arrows } /></a>
+        <div className="menu">
+          <div className="textMenu">Кейсы
+            <div className="bottomLine"></div></div>
+          <div className="textMenu">Видеоблог
+            <div className="bottomLine"></div></div>
+          <div className="textMenu">Полезные статьи</div>
+        </div></div>
+
+      {/* <button
           className='nav_btn nav_close_btn'
           onClick={showNavbar}
         >
